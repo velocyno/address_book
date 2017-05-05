a866b34146df449aaca302e6166d7486eff0cdf9
Titus Fortner <titusfortner@gmail.com>
2017-05-05 15:31:43 -0500

    update model relationship and validation

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,11 @@
 class Address < ApplicationRecord
+  belongs_to :user
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :line_1, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip_code, presence: true
+
 end
