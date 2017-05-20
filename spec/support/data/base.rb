cc4834893224a2b50d1c1acaf6e5379d16b9c114
Titus Fortner <titusfortner@gmail.com>
2017-05-19 23:07:43 -0700

    remove unused code

--- spec/support/data/base.rb
+++ spec/support/data/base.rb
@@ -1,20 +1,5 @@
-require 'active_support/hash_with_indifferent_access'
-
 class BaseModel < WatirModel
 
   attr_accessor :id
-
-  def self.convert(hash, *args)
-    hash.deep_symbolize_keys!
-    id = hash.delete :id
-
-    model = super
-    model.tap { |m| m.id = id }
-  end
-
-  def eql?(other)
-    keys.all? {|k| send(k) == other.send(k)}
-  end
-  alias_method :==, :eql?
 
 end
