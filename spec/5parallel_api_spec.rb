62d3f7f3af6b279300cf85736c25a57dd29b593c
Titus Fortner <titusfortner@gmail.com>
2017-06-13 14:57:29 -0700

    don't keep initializing Site class

--- spec/5parallel_api_spec.rb
+++ spec/5parallel_api_spec.rb
@@ -6,71 +6,72 @@ module AddressBook
 
     let(:user) { Data::User.new }
     let(:address) { Data::Address.new }
+    let(:site) { Site.new }
 
     it 'signs up' do
       SignUp.visit.submit_form(user)
-      expect(Site.new.logged_in?(user)).to eq true
+      expect(site.logged_in?(user)).to eq true
     end
 
     it 'login' do
-      Site.new.create_user(user)
+      site.create_user(user)
 
       SignIn.visit.submit_form(user)
 
-      expect(Site.new.logged_in?(user)).to eq true
+      expect(site.logged_in?(user)).to eq true
     end
 
     it 'logout' do
-      Site.new.log_in_user(user)
+      site.log_in_user(user)
 
       Home.visit.sign_out_user
 
-      expect(Site.new.logged_in?(user)).to eq false
+      expect(site.logged_in?(user)).to eq false
     end
 
     it 'creates address' do
-      Site.new.log_in_user(user)
+      site.log_in_user(user)
 
       AddressNew.visit.submit_form(address)
 
       expect(AddressShow.new.created_message?).to eq true
-      expect(Site.new.address?(address)).to eq true
+      expect(site.address?(address)).to eq true
     end
 
     it 'shows' do
-      Site.new.log_in_user(user)
-      Site.new.create_address(address)
+      site.log_in_user(user)
+      site.create_address(address)
 
       expect(AddressShow.visit(address).address?(address)).to eq true
     end
 
     it 'lists' do
-      Site.new.log_in_user(user)
-      Site.new.create_address(address)
-      address2 = Site.new.create_address
+      site.log_in_user(user)
+      site.create_address(address)
+      address2 = site.create_address
 
       expect(AddressList.visit.address?(address)).to eq true
       expect(AddressList.visit.address?(address2)).to eq true
     end
 
     it 'edits' do
-      Site.new.log_in_user(user)
-      Site.new.create_address(address)
+      site.log_in_user(user)
+      site.create_address(address)
 
       edited_address = AddressEdit.visit(address).submit_form
 
       expect(AddressShow.new.updated_message?).to eq true
-      expect(Site.new.address?(edited_address)).to eq true
+      expect(site.address?(edited_address)).to eq true
     end
 
     it 'deletes address' do
-      Site.new.log_in_user(user)
-      Site.new.create_address(address)
+      site.log_in_user(user)
+      site.create_address(address)
 
       AddressList.visit.destroy(address)
 
       expect(AddressList.new.destroyed_message?).to eq true
-      expect(Site.new.address?(address)).to eq false
+      expect(site.address?(address)).to eq false
     end
 
   end
