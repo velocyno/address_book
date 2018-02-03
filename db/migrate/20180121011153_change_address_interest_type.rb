8afcc679e561d9f34ce237adcd0a9bf2678ac5e9
Titus Fortner <titusfortner@gmail.com>
2018-02-03 11:54:12 -0600

    change migrations to force checkbox columns to be boolean

--- db/migrate/20180121011153_change_address_interest_type.rb
+++
@@ -1,7 +0,0 @@
-class ChangeAddressInterestType < ActiveRecord::Migration[5.0]
-  def change
-    change_column(:addresses, :interest_climb, :boolean)
-    change_column(:addresses, :interest_dance, :boolean)
-    change_column(:addresses, :interest_read, :boolean)
-  end
-end
