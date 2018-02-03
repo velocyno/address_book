4da58dedf8b61f4c898ded5dc3257356365dcb5e
Titus Fortner <titusfortner@gmail.com>
2018-02-03 11:45:08 -0600

    specify a using for changing type of table column

--- db/migrate/20180121011021_change_address_card_type.rb
+++ db/migrate/20180121011021_change_address_card_type.rb
@@ -1,5 +1,5 @@
 class ChangeAddressCardType < ActiveRecord::Migration[5.0]
   def change
-    change_column(:addresses, :card, :boolean)
+    change_column(:addresses, :card, :boolean, 'USING card:boolean')
   end
 end
